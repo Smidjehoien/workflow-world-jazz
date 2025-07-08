1501cf8302eb87b9870ce6f8573bc9263342656a
Pranay Prakash <pranay.gp@gmail.com>
2025-07-08 09:43:58 -0700

    reduce step fatal error

--- apps/example/workflows/steps.ts
+++ apps/example/workflows/steps.ts
@@ -3,11 +3,15 @@ import { FatalError } from '@vercel/workflow-core';
 export async function add(a: number, b: number): Promise<number> {
   'use step';
 
+  // Mimic a retryable error 50% of the time
   if (Math.random() < 0.5) {
     throw new Error('Retryable error');
   }
-  if (Math.random() < 0.2) {
+
+  // Mimic a 5% chance of the workflow actually failing
+  if (Math.random() < 0.05) {
     throw new FatalError("We're cooked yo!");
   }
+
   return a + b;
 }
