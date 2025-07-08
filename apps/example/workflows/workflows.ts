72c915d6f06bfbfc1b48d778c786108036683a56
Nathan Rajlich <n@n8.io>
2025-07-08 16:27:30 -0700

    Remove `sleep()` call from example workflow

--- apps/example/workflows/workflows.ts
+++ apps/example/workflows/workflows.ts
@@ -1,11 +1,11 @@
-import { sleep } from '@vercel/workflow-core';
+//import { sleep } from '@vercel/workflow-core';
 import { add } from './steps.ts';
 
 export async function example(i: number) {
   'use workflow';
   const a = await add(i, 7);
   const b = await add(a, 8);
-  await sleep('1 minute');
+  //await sleep('1 minute');
   const c = await add(b, 9);
   return c;
 }
