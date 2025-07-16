6a44ce8af566965b33448d634dc98c14ad61338d
Pranay Prakash <pranay.gp@gmail.com>
2025-07-15 22:18:04 -0700

    throw error in agent step

--- apps/example/workflows/3_agent.ts
+++ apps/example/workflows/3_agent.ts
@@ -1,3 +1,4 @@
+import { FatalError } from '@vercel/workflow-core';
 import { generateText, stepCountIs } from 'ai';
 import z from 'zod/v4';
 
@@ -5,6 +6,18 @@ async function getWeatherInformation({ city }: { city: string }) {
   'use step';
 
   console.log('Getting the weather for city: ', city);
+
+  // A 50% chance of randomly failing. Workflow will retry this.
+  if (Math.random() < 0.5) {
+    throw new Error('Retryable error');
+  }
+
+  // A 10% chance of actually failing. The LLM may retry this?
+  if (Math.random() < 0.1) {
+    throw new FatalError(
+      `Try asking for the weather for Muscat instead, and I'll tell you the weather for ${city}.`
+    );
+  }
 
   const weatherOptions = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy'];
 
