e57b07299939e45e1f99c301ac589645d305ea63
Pranay Prakash <pranay.gp@gmail.com>
2025-09-10 16:23:05 -0700

    fix sleeping workflow name conflict

--- apps/nextjs/app/api/signup/route.ts
+++
@@ -1,9 +0,0 @@
-import { example } from '@/workflows/workflows';
-
-export async function POST() {
-  const { readable, writable } = new TransformStream();
-  const result = await example(writable); // start()
-  // => { runId: '123', status: 'queued' }
-  console.log('got result', { result });
-  return new Response(readable);
-}
--- apps/nextjs/app/server-action/actions.ts
+++
@@ -1,7 +0,0 @@
-'use server';
-import { example } from '../../workflows/workflows';
-
-export async function callWorkflow() {
-  const { writable } = new TransformStream();
-  return example(writable);
-}
