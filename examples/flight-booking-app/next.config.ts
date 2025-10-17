30414df5ce9f1c12d6bc9e6aace9f01cf2b0942c
Adrian <me@adriandlam.com>
2025-10-17 11:27:59 -0700

    add nextjs-turbopack workbench app as an example app (#364)
    * add nextjs-turbopack workbench app as an example app
    
    * refactor workflows in example app and fix releasing writable stream early
    
    * update README for turbo dev in examples
    
    * simplify next config

--- examples/flight-booking-app/next.config.ts
+++ examples/flight-booking-app/next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // for easier debugging
-  experimental: {
-    serverMinification: false,
-  },
 };
 
-// export default nextConfig;
 export default withWorkflow(nextConfig);
